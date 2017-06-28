9f5cfcb881e358afca85a987de2ccf037cde672c
arthur <wsz@jxgi.com>
2017-06-28 09:55:50 +0100

    - Code Review

--- app/src/main/java/delivery/com/consts/DBConsts.java
+++ app/src/main/java/delivery/com/consts/DBConsts.java
@@ -6,9 +6,6 @@ package delivery.com.consts;
 
 public class DBConsts {
 
-    public static final String TABLE_NAME_DESPATCH = "tbl_despatch";
-    public static final String TABLE_NAME_OUTLET = "tbl_outlet";
-    public static final String TABLE_NAME_TIER = "tbl_tier";
     public static final String TABLE_NAME_STOCK = "tbl_stock";
     public static final String TABLE_NAME_WAREHOUSE = "tbl_warehouse";
     public static final String TABLE_NAME_STAFF = "tbl_staff";
@@ -17,12 +14,6 @@ public class DBConsts {
     public static final String TABLE_NAME_BAY = "tbl_bay";
 
     public static final String FIELD_ID = "id";
-    public static final String FIELD_DESPATCH_ID = "despatch_id";
-    public static final String FIELD_RUN = "run";
-    public static final String FIELD_DRIVER = "driver";
-    public static final String FIELD_ROUTE = "route";
-    public static final String FIELD_REG = "reg";
-    public static final String FIELD_DATE = "created_at";
     public static final String FIELD_COMPLETED = "completed";
 
     public static final String FIELD_WAREHOUSE_ID = "warehouse_id";
@@ -40,20 +31,6 @@ public class DBConsts {
 
     public static final String FIELD_BAY_ID = "bay_id";
     public static final String FIELD_BAY = "bay";
-
-    public static final String FIELD_OUTLET_ID = "outlet_id";
-    public static final String FIELD_OUTLET = "outlet";
-    public static final String FIELD_ADDRESS = "address";
-    public static final String FIELD_SERVICE = "service";
-    public static final String FIELD_DELIVER_TIME = "deliver_time";
-    public static final String FIELD_DELIVERED = "delivered";
-    public static final String FIELD_TIERS = "tiers";
-    public static final String FIELD_REASON = "reason";
-
-    public static final String FIELD_TIER_NO = "tier_no";
-    public static final String FIELD_TIER_SPACE = "tier_space";
-    public static final String FIELD_SLOTS = "slots";
-    public static final String FIELD_TIER_ORDER = "tier_order";
 
     public static final String FIELD_STOCK_ID = "stock_id";
     public static final String FIELD_TITLE_ID = "title_id";
@@ -76,15 +53,4 @@ public class DBConsts {
     public static final String FIELD_NEW_ZONE = "new_zone";
     public static final String FIELD_NEW_BAY = "new_bay";
     public static final String FIELD_DATE_TIMESTAMP = "datetimestamp";
-
-    public static final String FIELD_STOCK = "stock";
-    public static final String FIELD_TIER = "tier";
-    public static final String FIELD_SLOT = "slot";
-    public static final String FIELD_QTY = "qty";
-
-    public static final String FIELD_REMOVE = "remove";
-    public static final String FIELD_REMOVE_ID = "remove_id";
-    public static final String FIELD_SIZE = "size";
-    public static final String FIELD_SLOT_ORDER = "slot_order";
-
 }
